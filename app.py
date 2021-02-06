a85dee7992ad9d8907bc5109d996a9cadcb33bca
Stephen Roille <stephen.roille@gmail.com>
2021-02-06 20:34:35 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import flask
+
+
+app = flask.Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
