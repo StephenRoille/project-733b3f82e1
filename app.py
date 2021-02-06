9cf691535fb06fb761134d427754bb21fc3594fe
Stephen Roille <stephen.roille@gmail.com>
2021-02-06 21:04:24 +0100

    Use environement variables Heroku service

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route("/", methods=["GET"])
 def home():
     logging.info("hello from the logger!")
-    return flask.render_template("index.html")
+    name = os.environ.get("NAME", "No Name provided")
+    return flask.render_template("index.html", name=name)
 
 
 if __name__ == "__main__":
